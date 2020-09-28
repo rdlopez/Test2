0f30d24791775eb24880552dfdc1647d8f807d00
Ruben Villota <get>
2020-09-28 13:04:11 -0500

    Attribute Concurrency

--- Domain/UploadFile.cs
+++ Domain/UploadFile.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Domain
 {
@@ -8,6 +6,8 @@ namespace Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [ConcurrencyCheck]
         public int Status { get; set; }
     }
 }
--- Persistence/Repository.cs
+++ Persistence/Repository.cs
@@ -55,6 +55,39 @@ namespace Persistence
             {
                 context.Update(entity);
                 await context.SaveChangesAsync();
+                //try
+                //{
+                //    context.Update(entity);
+                //    await context.SaveChangesAsync();
+                //}
+                //catch (DbUpdateConcurrencyException ex)
+                //{
+                //    Console.WriteLine(ex);
+                //    foreach (var entry in ex.Entries)
+                //    {
+                //        if (entry.Entity is UploadFile)
+                //        {
+                //            var proposedValues = entry.CurrentValues;
+                //            var databaseValues = entry.GetDatabaseValues();
+
+                //            foreach (var property in proposedValues.Properties)
+                //            {
+                //                Console.WriteLine(property);
+                //                var proposedValue = proposedValues[property];
+                //                var databaseValue = databaseValues[property];
+
+                //                // TODO: decide which value should be written to database
+                //                // proposedValues[property] = <value to be saved>;
+                //            }
+
+                //            // Refresh original values to bypass next concurrency check
+                //            entry.OriginalValues.SetValues(databaseValues);
+                //        }
+                //        else
+                //        {
+                //        }
+                //    }
+                //}
             });
         }
 
